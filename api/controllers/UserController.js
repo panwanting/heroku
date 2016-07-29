4f5be96ff5f9e1c103dae1063ae753836354279f
panwanting <pan19950529@gmail.com>
2016-07-29 07:03:47 +0000

    first commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,45 @@
+/**
+ * UserController
+ *
+ * 
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+                index: function (req,res){
+                                res.view('userindex');
+                },
+                list: function(req,res){
+                                User.find()
+                                .then(function(users){
+                                                res.view('userlist',{
+                                                        users: users
+                                                });
+                                });
+                },
+                page: function(req,res){
+                        res.view("userpage");
+                },
+                create:function(req,res){
+                        var name = req.body.name;
+                        User.create({
+                                name: name
+                        })
+                        .then(function (user){
+                                res.redirect("/user/list");
+                        })
+                },
+                
+                  destroy: function(req,res){
+                        var id = req.params['id'];
+                        User.destroy({
+                                
+                                id: id
+                        })
+                        .then(function(user){
+                                res.redirect("/user/list");
+                        });
+                  }
+};
+
